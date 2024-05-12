6a5e8edee5be9bd43739ea2d9e8625f9d73dcf35
Matias Cabrera <matias.clavijo@koin.com.br>
2024-05-12 18:08:02 -0300

    added side nav

--- src/app/components/basics/variable-card/variable-card.component.ts
+++ src/app/components/basics/variable-card/variable-card.component.ts
@@ -1,12 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { BadgeComponent } from '../badge/badge.component';
+import { NgbTooltipModule } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-variable-card',
   standalone: true,
-  imports: [],
+  imports: [BadgeComponent, NgbTooltipModule],
   templateUrl: './variable-card.component.html',
   styleUrl: './variable-card.component.scss'
 })
 export class VariableCardComponent {
-
+  @Input() title?: string;
+  @Input() tooltipInfo?: string;
+  @Input() labelTitle?: string;
+  @Input() labelColor?: string;
 }
