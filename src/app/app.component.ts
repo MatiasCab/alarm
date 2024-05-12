5ac635cc92ae1b0a09e9f608177cc3d6e8fffc8c
Matias Cabrera <matias.clavijo@koin.com.br>
2024-05-12 15:35:23 -0300

    structure

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { VariableCardComponent } from './components/basics/variable-card/variable-card.component';
+import { SideNavComponent } from './components/basics/side-nav/side-nav.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, VariableCardComponent, SideNavComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
